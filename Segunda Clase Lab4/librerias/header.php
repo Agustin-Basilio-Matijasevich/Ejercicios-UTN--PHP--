2a7efb9866968d396ba5718d466fa1c2d979f7e4
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-08-18 21:42:19 -0300

    cambios segunda clase

---
+++ Segunda Clase Lab4/librerias/header.php
@@ -0,0 +1,14 @@
+<div id="header">
+      <div id="logo">
+        <h1>logo here</h1>
+        <h4>have your punchline here</h4>
+      </div>
+      <div id="links">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Technology</a></li>
+          <li><a href="#">Contact us</a></li>
+        </ul>
+      </div>
+    </div>
